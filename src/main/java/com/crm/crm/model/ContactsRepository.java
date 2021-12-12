001ce6ceb71e045cddecc007068fbc5db3937fc4
Pavel Shakhlovich <Altmersc@gmail.com>
2021-12-12 03:06:41 +0300

    CORS

--- src/main/java/com/crm/crm/model/ContactsRepository.java
+++ src/main/java/com/crm/crm/model/ContactsRepository.java
@@ -1,5 +1,7 @@
 package com.crm.crm.model;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin
 public interface ContactsRepository extends CrudRepository<Contact, Long> {}
--- src/main/java/com/crm/crm/model/DemoLoader.java
+++ src/main/java/com/crm/crm/model/DemoLoader.java
@@ -1,5 +1,6 @@
 package com.crm.crm.model;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,7 +13,11 @@ public class DemoLoader implements CommandLineRunner {
 
   @Override
   public void run(String... args) throws Exception {
-    contactsRepository.save(
-        Contact.builder().firstName("Pavel").lastName("Shakhlovich").email("pavel@myEmail.com").build());
+    contactsRepository.saveAll(
+        List.of(
+            Contact.builder().firstName("Pavel").lastName("Shakhlovich").email("pavel@myEmail.com").build(),
+            Contact.builder().firstName("Emmanuel").lastName("Henri").email("emmanuel@myEmail.com").build(),
+            Contact.builder().firstName("Pedro").lastName("Rodrigues").email("pedro@myEmail.com").build(),
+            Contact.builder().firstName("Kim").lastName("Lee").email("kim@myEmail.com").build()));
   }
 }
